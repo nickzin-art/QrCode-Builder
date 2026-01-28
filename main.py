0f4bcd6429313d4098a0ffeeef27604039683f80
nickzin-art <nicolasbez2009.11@gmail.com>
2026-01-28 10:40:42 -0300

    commit 95% pronto

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.uic import loadUi
-from pyshorteners import Shortener
 from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtGui import QPixmap
+from pyshorteners import Shortener
+import qrcode
+import os
 
 class TelaPrograma(QMainWindow):
     def __init__(self):
@@ -9,29 +12,36 @@ class TelaPrograma(QMainWindow):
         loadUi("GEAR_URL.ui", self)
         
         self.shortener = Shortener()
-        
         self.btn_gear.clicked.connect(self.on_btn_gear_clicked)
 
     def get_url(self):
-        return self.site_url.text()
+        return self.insert_url.text()
         
     def set_url_curta(self, url):
         self.site_url.setText(url)
-    
-    @pyqtSlot()    
+
+    def gerar_qrcode(self, url):
+        img = qrcode.make(url)
+        caminho = "qrcode.png"
+        img.save(caminho)
+
+        pixmap = QPixmap(caminho)
+        url.lbl_qr.setPixmap(pixmap)
+        url.lbl_qr.setScaledContents(True)
+
+    @pyqtSlot()
     def on_btn_gear_clicked(self):
         valor = self.get_url()
         try:
             url_curta = self.shortener.tinyurl.short(valor)
             self.set_url_curta(url_curta)
+            # self.gerar_qrcode(url_curta)
         except Exception as e:
             print(f"Erro ao gerar URL: {e}")
             self.set_url_curta("Erro: URL inválida")
 
 if __name__ == "__main__":
     app = QApplication([])
-
     tela = TelaPrograma()
     tela.show()
-
     app.exec_()
