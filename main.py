e4960b1e5a551327eb5579b909b97a23b580d1d1
nickzin-art <nicolasbez2009.11@gmail.com>
2026-01-30 09:27:50 -0300

    commit Finalizado

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from PyQt5.uic import loadUi
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtGui import QPixmap
 from pyshorteners import Shortener
+from PyQt5.QtCore import QDateTime
+from PyQt5 import QtWidgets
+from PyQt5.QtGui import QPainter
 import qrcode
 import os
+from os import path
 import sys
-
-def GET_PATH(myPath):
-    return f''
 
 
 class TelaPrograma(QMainWindow):
@@ -18,7 +19,8 @@ class TelaPrograma(QMainWindow):
         
         self.shortener = Shortener()
         self.btn_gear.clicked.connect(self.on_btn_gear_clicked)
-        #self.btn_save.clicked.connect(self.on_btnSalvar_clicked)
+        self.btn_save.clicked.connect(self.on_btnSalvar_clicked)
+        self.btn_save.setEnabled(True)
 
     def get_url(self):
         return self.insert_url.text()
@@ -35,6 +37,32 @@ class TelaPrograma(QMainWindow):
         self.lbl_qr.setPixmap(pixmap)
         self.lbl_qr.setScaledContents(True)
 
+    def salvar(self):
+        nome_arquivo , _ = QFileDialog.getSaveFileName(self, "Salvar Imagem")
+        if nome_arquivo:
+            caminho = path.dirname(nome_arquivo)
+            nome = nome_arquivo.removeprefix(caminho)
+
+            with open("qrcode.png", "rb") as fotoQr:
+                Qrcode = fotoQr.read()
+
+            with open(caminho+f"{nome}.png", "wb") as foto:
+                foto.write(Qrcode)
+
+            self.ShowMessage("Imagem", "Imagem salva com sucesso")
+            self.reset()
+
+
+    def reset(self):
+        self.btn_save.setEnabled(False)
+        self.insert_url.setText("")
+        self.site_url.setText("")
+        self.lbl_qr.clear()
+
+    def ShowMessage(self, title, message):
+        QMessageBox.information(self, title, message)
+
+
 
     @pyqtSlot()
     def on_btn_gear_clicked(self):
@@ -49,9 +77,16 @@ class TelaPrograma(QMainWindow):
             print(f"Erro ao gerar URL: {e}")
             self.set_url_curta("Erro: URL inválida")
     
-    #@pyqtSlot()
-    #def on_btnSalvar_clicked(self, url_curta):
-        #self.gerar_qrcode(url_curta)
+    @pyqtSlot()
+    def on_btnSalvar_clicked(self):
+        self.salvar()
+
+
+
+        
+
+        
+
         
 if __name__ == "__main__":
     app = QApplication([])
